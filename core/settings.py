b95051f53356f03630d56a46cf3e76204c5fc970
Thalisson Menezes <thalissons.menezes@gmail.com>
2026-01-14 14:17:10 -0300

    feat: app user with deafult model user

--- core/settings.py
+++ core/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+
+    # MY APPS
+    'apps.users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -86,6 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
