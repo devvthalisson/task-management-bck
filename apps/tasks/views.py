76b9e62d6e07b6052788b8173a7f8766e65ee579
Thalisson Menezes <thalissons.menezes@gmail.com>
2026-01-14 15:27:40 -0300

    feat: criar view para listagem e criação de tasks

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView
+from .models import Task
+from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class TaskListCreateAPIView(ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(owner=self.request.user)
