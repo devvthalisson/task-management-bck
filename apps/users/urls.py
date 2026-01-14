e7dfdd87ed19889d32c9e954109bfaf1f3cc0ed4
Thalisson Menezes <thalissons.menezes@gmail.com>
2026-01-14 14:47:39 -0300

    feat: adicionar endpoint put delete e retrieve

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import UserListCreateAPIView
+from .views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView
 
 
 urlpatterns = [
     path('users/', UserListCreateAPIView.as_view(), name='userListCreate'),
+    path(
+        'users/account/<int:pk>',
+        UserRetrieveUpdateDestroyAPIView.as_view(),
+        name='userRetrieveUpdateDestroyAPIView',
+    ),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,8 +1,13 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import UserSerializer
 from .models import User
 
 
 class UserListCreateAPIView(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
