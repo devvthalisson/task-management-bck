4e18fc62ae4dfaf7f5a8a1088c5ba05879b9c633
Thalisson Menezes <thalissons.menezes@gmail.com>
2026-01-14 15:19:38 -0300

    feat: model de task

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from apps.users.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
