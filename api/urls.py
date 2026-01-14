f5b056d1331c289ffb9647330bdcd0fd937f31c3
Thalisson Menezes <thalissons.menezes@gmail.com>
2026-01-14 15:30:23 -0300

    feat: adicionar rota content/tasks/

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('auth/', include('apps.users.urls'))
+    path('auth/', include('apps.users.urls')),
+    path('content/', include('apps.tasks.urls')),
 ]
---
+++ apps/tasks/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import TaskListCreateAPIView
+
+
+urlpatterns = [
+    path('tasks/', TaskListCreateAPIView.as_view(), name='taskListCreateAPIView'),
+]
