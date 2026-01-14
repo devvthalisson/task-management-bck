64fb71434a07e55da145def0c166223082717870
Thalisson Menezes <thalissons.menezes@gmail.com>
2026-01-14 14:39:37 -0300

    feat: adicionar verificação de password

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,6 +1,9 @@
 from typing import Dict, Any
 from rest_framework import serializers
 from .models import User
+from django.contrib.auth.password_validation import validate_password as django_validate_password
+from rest_framework.exceptions import ValidationError
+from django.core.exceptions import ValidationError as DjangoValidationError
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -18,3 +21,14 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validation_data:Dict[str, Any]) -> User:
         user = User.objects.create_user(**validation_data)
         return user
+    
+    def validate_password(self, value: User) -> User:
+        if value:
+            try:
+                django_validate_password(value)
+            except DjangoValidationError as e:
+                raise ValidationError(e.messages)
+            
+            return value
+        
+        raise ValidationError('Password is required')
