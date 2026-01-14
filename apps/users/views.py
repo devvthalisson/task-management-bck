9aa2a1d9abfb37f3aec6090e2c36a422985ac867
Thalisson Menezes <thalissons.menezes@gmail.com>
2026-01-14 15:00:14 -0300

    feat: restringir rotas dos usuários

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,13 +1,21 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import UserSerializer
 from .models import User
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 
 
 class UserListCreateAPIView(ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def get_permissions(self):
+        self.permission_classes = [IsAdminUser]
+        if self.request.method == 'POST':
+            self.permission_classes = [AllowAny]
+        
+        return super().get_permissions()
 
 class UserRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminUser]
