3d2efd5d59059ef72f3d129d947a5991c370acf9
Thalisson Menezes <thalissons.menezes@gmail.com>
2026-01-16 10:57:25 -0300

    feat: setup pagination class

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'MAX_PAGE_SIZE': 50,
 }
 
 SIMPLE_JWT = {
